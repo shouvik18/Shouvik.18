43933d6dca0cc458d1c1cf2fd9a296fd9b20c029
shouvik18 <shouvikpaulofficial@gmail.com>
2025-03-26 09:04:19 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="footer">
-      <p className="creator">&copy; {currentYear} Ngoako Ramokgopa</p>
+      <p className="creator">&copy; {currentYear} Shouvik Pal</p>
     </footer>
   );
 }
